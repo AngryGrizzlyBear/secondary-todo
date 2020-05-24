e71d6be3473fe4eaa5f31702067b5976e71d83a3
Evan Longwood <evanmlongwood@gmail.com>
2020-05-24 16:39:35 -0700

    animations

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import FlipMove from "react-flip-move";
 
 class TodoItems extends Component {
   constructor(props) {
@@ -6,20 +7,29 @@ class TodoItems extends Component {
 
     this.createTasks = this.createTasks.bind(this);
   }
-  
+
   delete(key) {
-      this.props.delete(key);
+    this.props.delete(key);
   }
 
   createTasks(item) {
-    return <li onClick={() => this.delete(item.key)} 
-    key={item.key}>{item.text}</li>
+    return (
+      <li onClick={() => this.delete(item.key)} key={item.key}>
+        {item.text}
+      </li>
+    );
   }
 
   render() {
     let todoEntries = this.props.entries;
     let listItems = todoEntries.map(this.createTasks);
-    return <ul className="theList">{listItems}</ul>;
+    return (
+      <ul className="theList">
+        <FlipMove duration={250} easing="ease-out">
+          {listItems}
+        </FlipMove>
+      </ul>
+    );
   }
 }
 
